d9bc704b8e39ebb4ac87484c0e927604f9a45be1
Gabrielly Moura <gabriellymouta@gmail.com>
2022-07-24 15:27:20 -0300

    wip: estilo dinamico de botao

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
 const Container = () => {
   return (
-    <div className="container">
+    <ContainerWrapper>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </ContainerWrapper>
   );
 };
+
+const ContainerWrapper = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
 export default Container;
